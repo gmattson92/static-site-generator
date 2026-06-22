8052c30bfaae5a1978c3eb71c9765a9a596f572b
Greg Mattson <gregwmattson@gmail.com>
2026-06-22 12:07:22 -0500

    Added split_nodes_delimiter to split inline Markdown formatting into component TextNode objects. Added corresponding test cases.

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,86 +1,90 @@
 import unittest
-from textnode import TextNode, TextType, textnode_to_htmlnode
+import textnode as tn
 
 
 class TestTextNode(unittest.TestCase):
     def test_eq1(self):
-        node1 = TextNode('This is a text node', TextType.BOLD)
-        node2 = TextNode('This is a text node', TextType.BOLD)
+        node1 = tn.TextNode('This is a text node', tn.TextType.BOLD)
+        node2 = tn.TextNode('This is a text node', tn.TextType.BOLD)
         self.assertEqual(node1, node2)
 
     def test_eq2(self):
-        node1 = TextNode('This is a text node', TextType.BOLD, 'boot.dev')
-        node2 = TextNode('This is a text node', TextType.BOLD, 'boot.dev')
+        node1 = tn.TextNode('This is a text node',
+                            tn.TextType.BOLD, 'boot.dev')
+        node2 = tn.TextNode('This is a text node',
+                            tn.TextType.BOLD, 'boot.dev')
         self.assertEqual(node1, node2)
 
     def test_neq1(self):
-        node1 = TextNode('This is a text node', TextType.BOLD)
-        node2 = TextNode('This is a different text node', TextType.BOLD)
+        node1 = tn.TextNode('This is a text node', tn.TextType.BOLD)
+        node2 = tn.TextNode('This is a different text node', tn.TextType.BOLD)
         self.assertNotEqual(node1, node2)
 
     def test_neq2(self):
-        node1 = TextNode('This is a text node', TextType.BOLD)
-        node2 = TextNode('This is a text node', TextType.ITALIC)
+        node1 = tn.TextNode('This is a text node', tn.TextType.BOLD)
+        node2 = tn.TextNode('This is a text node', tn.TextType.ITALIC)
         self.assertNotEqual(node1, node2)
 
     def test_neq3(self):
-        node1 = TextNode('This is a text node', TextType.BOLD)
-        node2 = TextNode('This is a different text node', TextType.ITALIC)
+        node1 = tn.TextNode('This is a text node', tn.TextType.BOLD)
+        node2 = tn.TextNode('This is a different text node',
+                            tn.TextType.ITALIC)
         self.assertNotEqual(node1, node2)
 
     def test_neq4(self):
-        node1 = TextNode('This is a text node', TextType.BOLD)
-        node2 = TextNode('This is a text node', TextType.BOLD, 'boot.dev')
+        node1 = tn.TextNode('This is a text node', tn.TextType.BOLD)
+        node2 = tn.TextNode('This is a text node',
+                            tn.TextType.BOLD, 'boot.dev')
         self.assertNotEqual(node1, node2)
 
     def test_neq5(self):
-        node1 = TextNode('This is a text node', TextType.BOLD)
-        node2 = TextNode('This is a different text node',
-                         TextType.BOLD, 'boot.dev')
+        node1 = tn.TextNode('This is a text node', tn.TextType.BOLD)
+        node2 = tn.TextNode('This is a different text node',
+                            tn.TextType.BOLD, 'boot.dev')
         self.assertNotEqual(node1, node2)
 
     def test_neq6(self):
-        node1 = TextNode('This is a text node', TextType.BOLD)
-        node2 = TextNode('This is a different text node',
-                         TextType.ITALIC, 'boot.dev')
+        node1 = tn.TextNode('This is a text node', tn.TextType.BOLD)
+        node2 = tn.TextNode('This is a different text node',
+                            tn.TextType.ITALIC, 'boot.dev')
         self.assertNotEqual(node1, node2)
 
 
 class TestTextToHTML(unittest.TestCase):
     def test_plain(self):
-        node = TextNode('This is some plain text', TextType.PLAIN)
-        html_node = textnode_to_htmlnode(node)
+        node = tn.TextNode('This is some plain text', tn.TextType.PLAIN)
+        html_node = tn.textnode_to_htmlnode(node)
         self.assertEqual(html_node.tag, None)
         self.assertEqual(html_node.value, 'This is some plain text')
         self.assertEqual(html_node.to_html(), 'This is some plain text')
 
     def test_bold(self):
-        node = TextNode('This is some bold text', TextType.BOLD)
-        html_node = textnode_to_htmlnode(node)
+        node = tn.TextNode('This is some bold text', tn.TextType.BOLD)
+        html_node = tn.textnode_to_htmlnode(node)
         self.assertEqual(html_node.tag, 'b')
         self.assertEqual(html_node.value, 'This is some bold text')
         html_str = '<b>This is some bold text</b>'
         self.assertEqual(html_node.to_html(), html_str)
 
     def test_italic(self):
-        node = TextNode('This is some italic text', TextType.ITALIC)
-        html_node = textnode_to_htmlnode(node)
+        node = tn.TextNode('This is some italic text', tn.TextType.ITALIC)
+        html_node = tn.textnode_to_htmlnode(node)
         self.assertEqual(html_node.tag, 'i')
         self.assertEqual(html_node.value, 'This is some italic text')
         html_str = '<i>This is some italic text</i>'
         self.assertEqual(html_node.to_html(), html_str)
 
     def test_code(self):
-        node = TextNode('This is some code', TextType.CODE)
-        html_node = textnode_to_htmlnode(node)
+        node = tn.TextNode('This is some code', tn.TextType.CODE)
+        html_node = tn.textnode_to_htmlnode(node)
         self.assertEqual(html_node.tag, 'code')
         self.assertEqual(html_node.value, 'This is some code')
         html_str = '<code>This is some code</code>'
         self.assertEqual(html_node.to_html(), html_str)
 
     def test_link(self):
-        node = TextNode('This is a link', TextType.LINK, 'boot.dev')
-        html_node = textnode_to_htmlnode(node)
+        node = tn.TextNode('This is a link', tn.TextType.LINK, 'boot.dev')
+        html_node = tn.textnode_to_htmlnode(node)
         self.assertEqual(html_node.tag, 'a')
         self.assertEqual(html_node.value, 'This is a link')
         self.assertEqual(html_node.props, {'href': 'boot.dev'})
@@ -88,9 +92,9 @@ class TestTextToHTML(unittest.TestCase):
         self.assertEqual(html_node.to_html(), html_str)
 
     def test_image(self):
-        node = TextNode('This is an image', TextType.IMAGE,
-                        'boot.dev/dummy.jpg')
-        html_node = textnode_to_htmlnode(node)
+        node = tn.TextNode('This is an image', tn.TextType.IMAGE,
+                           'boot.dev/dummy.jpg')
+        html_node = tn.textnode_to_htmlnode(node)
         self.assertEqual(html_node.tag, 'img')
         self.assertEqual(html_node.value, '')
         self.assertEqual(html_node.props,
@@ -98,6 +102,81 @@ class TestTextToHTML(unittest.TestCase):
                           'alt': 'This is an image'})
         html_str = '<img src="boot.dev/dummy.jpg" alt="This is an image">'
         self.assertEqual(html_node.to_html(), html_str)
+
+
+class TestSplitNodes(unittest.TestCase):
+    def test_plain(self):
+        node = tn.TextNode('This is some plain text', tn.TextType.PLAIN)
+        new_nodes = tn.split_nodes_delimiter([node], '**')
+        self.assertEqual(new_nodes, [node])
+
+    def test_non_plain(self):
+        node = tn.TextNode('This is some plain text', tn.TextType.LINK)
+        new_nodes = tn.split_nodes_delimiter([node], '**')
+        self.assertEqual(new_nodes, [node])
+
+    def test_bold(self):
+        node = tn.TextNode('This is some **bold** text', tn.TextType.PLAIN)
+        new_nodes = tn.split_nodes_delimiter([node], '**')
+        correct = [
+            tn.TextNode('This is some ', tn.TextType.PLAIN),
+            tn.TextNode('bold', tn.TextType.BOLD),
+            tn.TextNode(' text', tn.TextType.PLAIN)
+        ]
+        self.assertEqual(new_nodes, correct)
+
+    def test_italic(self):
+        node = tn.TextNode('This is some _italic_ text', tn.TextType.PLAIN)
+        new_nodes = tn.split_nodes_delimiter([node], '_')
+        correct = [
+            tn.TextNode('This is some ', tn.TextType.PLAIN),
+            tn.TextNode('italic', tn.TextType.ITALIC),
+            tn.TextNode(' text', tn.TextType.PLAIN)
+        ]
+        self.assertEqual(new_nodes, correct)
+
+    def test_code(self):
+        node = tn.TextNode('This is some `code`', tn.TextType.PLAIN)
+        new_nodes = tn.split_nodes_delimiter([node], '`')
+        correct = [
+            tn.TextNode('This is some ', tn.TextType.PLAIN),
+            tn.TextNode('code', tn.TextType.CODE)
+        ]
+        self.assertEqual(new_nodes, correct)
+
+    def test_multi(self):
+        old_nodes = [
+            tn.TextNode('This is some **bold text**, ', tn.TextType.PLAIN),
+            tn.TextNode('followed by some `code`, ', tn.TextType.PLAIN),
+            tn.TextNode('then a link', tn.TextType.PLAIN, 'boot.dev')
+        ]
+        new_nodes = tn.split_nodes_delimiter(old_nodes, '`')
+        new_nodes = tn.split_nodes_delimiter(new_nodes, '**')
+        correct = [
+            tn.TextNode('This is some ', tn.TextType.PLAIN),
+            tn.TextNode('bold text', tn.TextType.BOLD),
+            tn.TextNode(', ', tn.TextType.PLAIN),
+            tn.TextNode('followed by some ', tn.TextType.PLAIN),
+            tn.TextNode('code', tn.TextType.CODE),
+            tn.TextNode(', ', tn.TextType.PLAIN),
+            tn.TextNode('then a link', tn.TextType.PLAIN, 'boot.dev')
+        ]
+        self.assertEqual(new_nodes, correct)
+
+    def test_concat(self):
+        original_text = ('This is some **bold text**, followed by some `code`'
+                         ', then a link')
+        node = tn.TextNode(original_text, tn.TextType.PLAIN, 'boot.dev')
+        new_nodes = tn.split_nodes_delimiter([node], '`')
+        new_nodes = tn.split_nodes_delimiter(new_nodes, '**')
+        correct = [
+            tn.TextNode('This is some ', tn.TextType.PLAIN, 'boot.dev'),
+            tn.TextNode('bold text', tn.TextType.BOLD, 'boot.dev'),
+            tn.TextNode(', followed by some ', tn.TextType.PLAIN, 'boot.dev'),
+            tn.TextNode('code', tn.TextType.CODE, 'boot.dev'),
+            tn.TextNode(', then a link', tn.TextType.PLAIN, 'boot.dev'),
+        ]
+        self.assertEqual(new_nodes, correct)
 
 
 if __name__ == '__main__':
--- src/textnode.py
+++ src/textnode.py
@@ -51,3 +51,50 @@ def textnode_to_htmlnode(text_node: TextNode) -> LeafNode:
         value = ''
         props = {'src': text_node.url, 'alt': text_node.text}
     return LeafNode(tag, value, props)
+
+
+def split_node_delimiter(node: TextNode,
+                         delimiter: str) -> list[TextNode]:
+    """
+    Splits text with inline Markdown formatting into component TextNode
+    objects. Cannot handle nested inline elements,
+    e.g. "_some **bold** italic text_"
+    """
+    if delimiter not in ['**', '_', '`']:
+        raise ValueError(f'Invalid delimiter {delimiter}; '
+                         'split_node_delimiter handles only bold ("**"), '
+                         'italic ("_") and code ("`") delimiters.')
+    # If node is not plain text, do nothing to it
+    if node.text_type != TextType.PLAIN:
+        return [node]
+    d_delimiter_to_text_type = {'**': TextType.BOLD,
+                                '_': TextType.ITALIC,
+                                '`': TextType.CODE}
+    text_type = d_delimiter_to_text_type[delimiter]
+    new_nodes = []
+    parts = node.text.split(delimiter)
+    # Ensure we have a matching closing delimiter for each opening one
+    if len(parts) % 2 != 1:
+        raise ValueError(f'Invalid Markdown string {node.text}; '
+                         f'found opening delimiter {delimiter} with no '
+                         'matching close')
+    for i, part in enumerate(parts):
+        # Don't create a new node for empty strings
+        if part == '':
+            continue
+        # Ensure only text between delimiters is converted to text_type
+        if i % 2 == 1:
+            this_text_type = text_type
+        else:
+            this_text_type = TextType.PLAIN
+        new_node = TextNode(part, this_text_type, node.url)
+        new_nodes.append(new_node)
+    return new_nodes
+
+
+def split_nodes_delimiter(old_nodes: list[TextNode],
+                          delimiter: str) -> list[TextNode]:
+    new_nodes = []
+    for node in old_nodes:
+        new_nodes.extend(split_node_delimiter(node, delimiter))
+    return new_nodes
